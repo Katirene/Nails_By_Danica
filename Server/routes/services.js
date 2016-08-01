90cbfd80d0aab6687d0b7b597798423f8e7c6183
Katirene <kat.traxler@gmail.com>
2016-08-01 16:13:51 -0500

    corrected environment variables

--- Server/routes/services.js
+++ Server/routes/services.js
@@ -5,7 +5,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var dotenv = require('dotenv');
 
-mongoose.connect('MONGODB_URI');
+var mongo_uri = process.env.MONGOBD_URI;
+
+mongoose.connect(mongo_uri);
 mongoose.model(
     'Service',
     new Schema({
